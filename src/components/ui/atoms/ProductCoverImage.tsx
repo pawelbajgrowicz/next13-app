58ba4c55f3523398edca116812fd768c65da7c90
Paweł Bajgrowicz <pawel.bajgrowicz@brandmed.pl>
2023-09-16 13:21:19 +0200

    Update code

---
+++ src/components/ui/atoms/ProductCoverImage.tsx
@@ -0,0 +1,9 @@
+export const ProductCoverImage = ({
+
+                                  }: {}) => {
+  return (
+    <div className={"aspect-square overflow-hidden rounded-md"}>
+
+    </div>
+  );
+};
