1826bf1c2b1f5f6551354cf214ad641057215017
Pawel Bajgrowicz <pawel.bajgrowicz@brandmed.pl>
2023-09-23 17:36:04 +0200

    Fix heading levels

--- src/ui/organisms/SingleProduct.tsx
+++ src/ui/organisms/SingleProduct.tsx
@@ -92,16 +92,16 @@ export const SingleProduct = ({
 					</div>
 
 					<div className="mt-8 flow-root sm:mt-12">
-						<h1 className="text-3xl font-bold">
+						<h2 className="text-3xl font-bold">
 							Delivered To Your Door
-						</h1>
+						</h2>
 						<p className="mt-4">
 							Lorem ipsum dolor sit amet consectetur adipisicing elit.
 							Quia accusantium nesciunt fuga.
 						</p>
-						<h1 className="mt-8 text-3xl font-bold">
+						<h2 className="mt-8 text-3xl font-bold">
 							From the Fine Farms of Brazil
-						</h1>
+						</h2>
 						<p className="mt-4">
 							Lorem ipsum dolor sit amet consectetur adipisicing elit.
 							Optio numquam enim facere.
